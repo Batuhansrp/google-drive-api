e4473a0ba10b98b4c5cb3ca0403899e201bc420f
Batuhansrp <68540266+Batuhansrp@users.noreply.github.com>
2020-12-08 08:31:49 +0300

    gdrive

---
+++ googleformdeneme/googleformdeneme/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace googleformdeneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.baglanbtn = new System.Windows.Forms.Button();
+            this.txtbox1 = new System.Windows.Forms.TextBox();
+            this.uploadbtn = new System.Windows.Forms.Button();
+            this.txtbox2 = new System.Windows.Forms.TextBox();
+            this.btnklasor = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // baglanbtn
+            // 
+            this.baglanbtn.Location = new System.Drawing.Point(52, 37);
+            this.baglanbtn.Name = "baglanbtn";
+            this.baglanbtn.Size = new System.Drawing.Size(656, 47);
+            this.baglanbtn.TabIndex = 0;
+            this.baglanbtn.Text = "Bağlan";
+            this.baglanbtn.UseVisualStyleBackColor = true;
+            this.baglanbtn.Click += new System.EventHandler(this.baglanbtn_Click);
+            // 
+            // txtbox1
+            // 
+            this.txtbox1.Location = new System.Drawing.Point(52, 135);
+            this.txtbox1.Multiline = true;
+            this.txtbox1.Name = "txtbox1";
+            this.txtbox1.Size = new System.Drawing.Size(342, 37);
+            this.txtbox1.TabIndex = 1;
+            this.txtbox1.Text = "Dosya yolunu giriniz";
+            // 
+            // uploadbtn
+            // 
+            this.uploadbtn.Location = new System.Drawing.Point(499, 128);
+            this.uploadbtn.Name = "uploadbtn";
+            this.uploadbtn.Size = new System.Drawing.Size(193, 44);
+            this.uploadbtn.TabIndex = 2;
+            this.uploadbtn.Text = "Dosya Yükle";
+            this.uploadbtn.UseVisualStyleBackColor = true;
+            this.uploadbtn.Click += new System.EventHandler(this.uploadbtn_Click);
+            // 
+            // txtbox2
+            // 
+            this.txtbox2.Location = new System.Drawing.Point(52, 226);
+            this.txtbox2.Multiline = true;
+            this.txtbox2.Name = "txtbox2";
+            this.txtbox2.Size = new System.Drawing.Size(342, 37);
+            this.txtbox2.TabIndex = 3;
+            // 
+            // btnklasor
+            // 
+            this.btnklasor.Location = new System.Drawing.Point(499, 219);
+            this.btnklasor.Name = "btnklasor";
+            this.btnklasor.Size = new System.Drawing.Size(193, 44);
+            this.btnklasor.TabIndex = 4;
+            this.btnklasor.Text = "Klasör Yarat";
+            this.btnklasor.UseVisualStyleBackColor = true;
+            this.btnklasor.Click += new System.EventHandler(this.btnklasor_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(63, 316);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(658, 276);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 615);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnklasor);
+            this.Controls.Add(this.txtbox2);
+            this.Controls.Add(this.uploadbtn);
+            this.Controls.Add(this.txtbox1);
+            this.Controls.Add(this.baglanbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button baglanbtn;
+        private System.Windows.Forms.TextBox txtbox1;
+        private System.Windows.Forms.Button uploadbtn;
+        private System.Windows.Forms.TextBox txtbox2;
+        private System.Windows.Forms.Button btnklasor;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ googleformdeneme/googleformdeneme/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+
+namespace googleformdeneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string[] Scopes = { DriveService.Scope.Drive };
+        static string ApplicationName = "googledeneme";
+        private void baglanbtn_Click(object sender, EventArgs e)
+        {
+            UserCredential credential;
+
+            credential = GetCredentials();
+
+            // Yeni drive api servisi yaratma
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            string pageToken = null;
+
+            //Dosyaları Listeleme
+                ListFiles(service, ref pageToken);
+
+           
+
+            
+
+        }
+
+        //credentials dosyasını okuma
+        private  UserCredential GetCredentials()
+        {
+            UserCredential credential;
+
+            using (FileStream stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+
+                credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+               
+            }
+
+            return credential;
+        }
+        private static void UploadBasicImage(string path, DriveService service)
+        {
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File();
+            fileMetadata.Name = Path.GetFileName(path);
+            fileMetadata.MimeType = "image/jpeg";
+            FilesResource.CreateMediaUpload request;
+            using (var stream = new System.IO.FileStream(path, System.IO.FileMode.Open))
+            {
+                request = service.Files.Create(fileMetadata, stream, "image/jpeg");
+                request.Fields = "id";
+                request.Upload();
+            }
+
+            var file = request.ResponseBody;
+
+            
+
+        }
+
+        private void uploadbtn_Click(object sender, EventArgs e)
+        {
+            UserCredential credential;
+
+            credential = GetCredentials();
+
+            
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            UploadBasicImage(txtbox1.Text, service);
+
+        }
+
+        private void btnklasor_Click(object sender, EventArgs e)
+        {
+            UserCredential credential;
+
+            credential = GetCredentials();
+
+           
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            string DirectoryName = txtbox2.Text;
+            var body = new Google.Apis.Drive.v3.Data.File();
+            body.Name = DirectoryName;
+            body.MimeType = "application/vnd.google-apps.folder";
+            
+            
+                var request = service.Files.Create(body);
+                request.Fields = "id";
+                var _FF = request.Execute();
+            
+        }
+
+        private void ListFiles(DriveService service, ref string pageToken)
+        {  
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 30;
+           
+            listRequest.Fields = "nextPageToken, files(name)";
+            listRequest.PageToken = pageToken;
+            listRequest.Q = "mimeType='image/jpeg'";
+
+            
+            var request = listRequest.Execute();
+
+
+            if (request.Files != null && request.Files.Count > 0)
+            {
+
+
+                foreach (var file in request.Files)
+                {
+                   listBox1.Items.Add( file.Name);
+                }
+
+                pageToken = request.NextPageToken;
+
+                if (request.NextPageToken != null)
+                {
+                    Console.WriteLine("Press any key to conti...");
+                    Console.ReadLine();
+
+
+
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("No files found.");
+
+            }
+
+        }
+    }
+}
